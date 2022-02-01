63dc28b369f19f192cb9f10df68a51405eab3c50
Kavindu Gimhan Zoysa <kavindu175@gmail.com>
2022-02-01 09:58:02 +0530

    Use data.rollbackCountWithinBlock instead of global var rollbackCountWithinBlock

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/CodeAnalyzer.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/semantics/analyzer/CodeAnalyzer.java
@@ -294,7 +294,6 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
     private final Names names;
     private final Stack<LinkedHashSet<BType>> returnTypes = new Stack<>();
     private final Stack<LinkedHashSet<BType>> errorTypes = new Stack<>();
-    private int rollbackCountWithinBlock;
     private final Map<BSymbol, Set<BLangNode>> workerReferences = new HashMap<>();
     private final ReachabilityAnalyzer reachabilityAnalyzer;
 
@@ -655,7 +654,7 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
     @Override
     public void visit(BLangRollback rollbackNode, AnalyzerData data) {
         data.rollbackCount++;
-        this.rollbackCountWithinBlock++;
+        data.rollbackCountWithinBlock++;
         if (data.transactionCount == 0 && !data.withinTransactionScope) {
             this.dlog.error(rollbackNode.pos, DiagnosticErrorCode.ROLLBACK_CANNOT_BE_OUTSIDE_TRANSACTION_BLOCK);
             return;
@@ -711,9 +710,9 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
     @Override
     public void visit(BLangBlockStmt blockNode, AnalyzerData data) {
         int prevCommitCount = data.commitCountWithinBlock;
-        int prevRollbackCount = this.rollbackCountWithinBlock;
+        int prevRollbackCount = data.rollbackCountWithinBlock;
         data.commitCountWithinBlock = 0;
-        this.rollbackCountWithinBlock = 0;
+        data.rollbackCountWithinBlock = 0;
         boolean inInternallyDefinedBlockStmt = data.inInternallyDefinedBlockStmt;
         data.inInternallyDefinedBlockStmt = checkBlockIsAnInternalBlockInImmediateFunctionBody(blockNode);
         final SymbolEnv blockEnv = SymbolEnv.createBlockEnv(blockNode, data.env);
@@ -721,11 +720,11 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
             analyzeNodeWithEnv(e, blockEnv, data);
         });
         data.inInternallyDefinedBlockStmt = inInternallyDefinedBlockStmt;
-        if (data.commitCountWithinBlock > 1 || rollbackCountWithinBlock > 1) {
+        if (data.commitCountWithinBlock > 1 || data.rollbackCountWithinBlock > 1) {
             this.dlog.error(blockNode.pos, DiagnosticErrorCode.MAX_ONE_COMMIT_ROLLBACK_ALLOWED_WITHIN_A_BRANCH);
         }
         data.commitCountWithinBlock = prevCommitCount;
-        this.rollbackCountWithinBlock = prevRollbackCount;
+        data.rollbackCountWithinBlock = prevRollbackCount;
     }
 
     private boolean checkBlockIsAnInternalBlockInImmediateFunctionBody(BLangNode node) {
@@ -2712,7 +2711,8 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
 
     // Asynchronous Send Statement
     public void visit(BLangWorkerSend workerSendNode, AnalyzerData data) {
-        BSymbol receiver = symResolver.lookupSymbolInMainSpace(data.env, names.fromIdNode(workerSendNode.workerIdentifier));
+        BSymbol receiver =
+                symResolver.lookupSymbolInMainSpace(data.env, names.fromIdNode(workerSendNode.workerIdentifier));
         if ((receiver.tag & SymTag.VARIABLE) != SymTag.VARIABLE) {
             receiver = symTable.notFoundSymbol;
         }
@@ -2773,7 +2773,8 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
 
     @Override
     public void visit(BLangWorkerSyncSendExpr syncSendExpr, AnalyzerData data) {
-        BSymbol receiver = symResolver.lookupSymbolInMainSpace(data.env, names.fromIdNode(syncSendExpr.workerIdentifier));
+        BSymbol receiver =
+                symResolver.lookupSymbolInMainSpace(data.env, names.fromIdNode(syncSendExpr.workerIdentifier));
         if ((receiver.tag & SymTag.VARIABLE) != SymTag.VARIABLE) {
             receiver = symTable.notFoundSymbol;
         }
@@ -2803,7 +2804,8 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
     public void visit(BLangWorkerReceive workerReceiveNode, AnalyzerData data) {
         // Validate worker receive
         validateActionParentNode(workerReceiveNode.pos, workerReceiveNode);
-        BSymbol sender = symResolver.lookupSymbolInMainSpace(data.env, names.fromIdNode(workerReceiveNode.workerIdentifier));
+        BSymbol sender =
+                symResolver.lookupSymbolInMainSpace(data.env, names.fromIdNode(workerReceiveNode.workerIdentifier));
         if ((sender.tag & SymTag.VARIABLE) != SymTag.VARIABLE) {
             sender = symTable.notFoundSymbol;
         }
@@ -4722,5 +4724,6 @@ public class CodeAnalyzer extends SimpleBLangNodeAnalyzer<CodeAnalyzer.AnalyzerD
         boolean withinTransactionScope;
         boolean commitRollbackAllowed;
         int commitCountWithinBlock;
+        int rollbackCountWithinBlock;
     }
 }
