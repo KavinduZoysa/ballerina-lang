ccc3bbe040305cc717f780d1284e7695b2689600
suleka96 <sulekahelmini96comp@gmail.com>
2022-08-10 06:43:42 +0530

    Add more tests and refactor code

--- misc/formatter/modules/formatter-core/src/test/java/org/ballerinalang/formatter/core/ParserTestFormatter.java
+++ misc/formatter/modules/formatter-core/src/test/java/org/ballerinalang/formatter/core/ParserTestFormatter.java
@@ -183,8 +183,7 @@ public class ParserTestFormatter extends FormatterTest {
                 "client_decl_source_08.bal", "client_decl_source_09.bal", "client_decl_source_13.bal",
                 "client_decl_source_14.bal", "invalid_usage_of_client_keyword_as_identifier_source.bal",
                 "resiliency_source_05.bal", "regexp_constructor_source_26.bal", "regexp_constructor_source_28.bal",
-                "query_expr_source_100.bal", "query_expr_source_89.bal",
-                "query_expr_source_96.bal");
+                "query_expr_source_105.bal", "query_expr_source_94.bal", "query_expr_source_101.bal");
     }
 
     @DataProvider(name = "test-file-provider")
