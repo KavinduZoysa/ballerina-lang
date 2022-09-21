caff99d8a05b44adb5110755eba113dc8a934730
suleka96 <sulekahelmini96comp@gmail.com>
2022-09-21 14:50:19 +0530

    Refactor code

--- compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/desugar/ConstantPropagation.java
+++ compiler/ballerina-lang/src/main/java/org/wso2/ballerinalang/compiler/desugar/ConstantPropagation.java
@@ -1022,12 +1022,7 @@ public class ConstantPropagation extends BLangNodeVisitor {
 
     @Override
     public void visit(BLangGroupingKey groupingKey) {
-        BLangNode groupingKeyNode = (BLangNode) groupingKey.getGroupingKey();
-        if (groupingKeyNode.getKind() == NodeKind.SIMPLE_VARIABLE_REF) {
-            groupingKey.variableRef = rewrite(groupingKey.variableRef);
-        } else {
-            groupingKey.variableDef = rewrite(groupingKey.variableDef);
-        }
+        groupingKey.setGroupingKey(rewrite((BLangNode) groupingKey.getGroupingKey()));
         result = groupingKey;
     }
 
